15855a53f2c0f8a7827a924c9b055368a2ac0f25
Masaood Shah <masaoodshah803@gmail.com>
2024-06-01 01:45:52 +0500

    add assignment images

---
+++ script.js
@@ -0,0 +1,9 @@
+var userBalance = +prompt("Enter your balance")
+
+if(userBalance >= 50){
+    alert("Welcome To ZU Peshawar")
+}
+
+if(userBalance < 50){
+    alert("Please recharge your balance")
+}
